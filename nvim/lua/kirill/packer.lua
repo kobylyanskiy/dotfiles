0faf156606fd545e2e4e665367a52cdacb959d82
Kirill Kobylyanskiy <k.kobylyanskiy@gmail.com>
2024-07-05 21:27:25 +0200

    Update packer.lua

--- nvim/lua/kirill/packer.lua
+++ nvim/lua/kirill/packer.lua
@@ -8,17 +8,161 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.6',
-	  requires = { {'nvim-lua/plenary.nvim'} }
+	  'nvim-telescope/telescope.nvim',
+	  requires = {
+		{'nvim-lua/plenary.nvim'},
+		{ "ANGkeith/telescope-terraform-doc.nvim" },
+		{ "nvim-telescope/telescope-live-grep-args.nvim" },
+	  },
+	  config = function()
+		require("telescope").load_extension("live_grep_args")
+	  end
+  }
+
+  use {
+    "folke/zen-mode.nvim",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  }
+
+  use {
+    "kndndrj/nvim-dbee",
+    requires = {
+      "MunifTanjim/nui.nvim",
+    },
+    run = function()
+      -- Install tries to automatically detect the install method.
+      -- if it fails, try calling it with one of these parameters:
+      --    "curl", "wget", "bitsadmin", "go"
+      require("dbee").install()
+    end,
+    config = function()
+      require("dbee").setup(--[[optional config]])
+    end
+  }
+
+  use {"akinsho/toggleterm.nvim", tag = '*'}
+
+  use({
+    "stevearc/conform.nvim",
+    config = function()
+      require("conform").setup()
+    end,
+  })
+
+  -- Lua
+  use {
+    "folke/persistence.nvim",
+    event = "BufReadPre", -- this will only start session saving when an actual file was opened
+    opts = {
+      -- add any custom options here
+    }
   }
 
   use { "catppuccin/nvim", as = "catppuccin" }
+  use { 'junegunn/limelight.vim' }
 
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use {
+	'nvim-lualine/lualine.nvim',
+	requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  }
+
+  use {
+    'kristijanhusak/vim-dadbod-ui',
+	requires = { 'tpope/vim-dadbod' }
+  }
+
+  use {
+	'ibhagwan/fzf-lua',
+	requires = { "nvim-tree/nvim-web-devicons" }
+  }
+
+  -- use {
+  --   'VonHeikemen/fine-cmdline.nvim',
+  --   requires = {
+  --     {'MunifTanjim/nui.nvim'}
+  --   }
+  -- }
+
+  -- lazy.nvim
+  use {
+    "folke/noice.nvim",
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+    }
+  }
+
+  use {
+	"folke/trouble.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+	  -- your configuration comes here
+	  -- or leave it empty to use the default settings
+	  -- refer to the configuration section below
+	},
+  }
+
+  use {'akinsho/bufferline.nvim', tag = "*", requires = 'nvim-tree/nvim-web-devicons'}
+
+  use {'stevearc/dressing.nvim'}
+
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+        local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+        ts_update()
+    end,
+  }
+
+  use({
+	"nvim-treesitter/nvim-treesitter-textobjects",
+	after = "nvim-treesitter",
+	requires = "nvim-treesitter/nvim-treesitter",
+  })
+
+  use {
+  "folke/which-key.nvim",
+  config = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 1000
+    require("which-key").setup {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  end
+}
+
+  use {
+  "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+  }
+
   use('nvim-treesitter/playground')
-  use('theprimeagen/harpoon')
+
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
+  -- use('rcarriga/nvim-notify')
+
+
+  use 'ray-x/go.nvim'
+  use 'ray-x/guihua.lua' -- recommended if need floating window support
 
   use {
 	  'VonHeikemen/lsp-zero.nvim',
@@ -38,9 +182,17 @@ return require('packer').startup(function(use)
   }
 
   use {
-	  "williamboman/mason.nvim",
 	  "williamboman/mason-lspconfig.nvim",
 	  "neovim/nvim-lspconfig",
+  }
+
+  use {
+	  "williamboman/mason.nvim",
+      opts = {
+        ensure_installed = {
+            "gopls",
+        },
+      },
   }
 
   use {
@@ -78,5 +230,3 @@ return require('packer').startup(function(use)
   use "lewis6991/gitsigns.nvim"
 
 end)
-
-
