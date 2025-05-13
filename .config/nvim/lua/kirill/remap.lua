7aa04931977842e0ce811a24700ea62f8a8fea0c
Kirill Kobylianskii <kirill.kobylianskii@collect.ai>
2025-05-13 16:07:14 +0200

    improve startup experience

--- .config/nvim/lua/kirill/remap.lua
+++ .config/nvim/lua/kirill/remap.lua
@@ -19,3 +19,23 @@ vim.keymap.set("n", "<C-k>", ":cprev<CR>")
 -- sideways
 vim.keymap.set("n", "<C-h>", ":SidewaysLeft<CR>")
 vim.keymap.set("n", "<C-l>", ":SidewaysRight<CR>")
+
+-- Disable netrw
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.api.nvim_create_autocmd("VimEnter", {
+	callback = function()
+		-- local argc = vim.fn.argc()
+		local argv = vim.fn.argv()
+		if #argv == 0 then
+			-- No file passed to nvim: open Telescope
+			vim.cmd("silent! lua require('telescope.builtin').oldfiles()")
+		elseif #argv == 1 and vim.fn.isdirectory(argv[1]) == 1 then
+			-- Opened with `nvim .` or a directory
+			vim.cmd("cd " .. argv[1]) -- Change working directory
+			vim.cmd("silent! lua require('telescope.builtin').oldfiles()")
+		end
+		-- If file/dir passed, do nothing (nvim handles it)
+	end,
+})
