40d7b92bfe6caa567d1302579ed63c91b9888cf7
Kirill Kobylianskii <kirill.kobylianskii@traderepublic.com>
2026-09-06 18:43:24 +0200

    rename

--- .config/nvim/after/plugin/inc-rename.lua
+++ .config/nvim/after/plugin/inc-rename.lua
@@ -1,4 +1,4 @@
 -- Setup lives in lua/plugins/inc-rename.lua (opts). Just the keymap here.
 vim.keymap.set("n", "<leader>rn", function()
 	return ":IncRename " .. vim.fn.expand("<cword>")
-end, { expr = true })
+end, { expr = true, desc = "Rename" })
