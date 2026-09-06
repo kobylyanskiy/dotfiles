fb8672ed583511c2bb210f9255f191b867f0ab77
Kirill Kobylianskii <kirill.kobylianskii@traderepublic.com>
2026-09-06 18:33:46 +0200

    marks

--- .config/nvim/after/plugin/marks.lua
+++
@@ -1,8 +0,0 @@
-require("marks").setup({
-	-- builtin_marks = { "<", ">" },
-	builtin_marks = {},
-	excluded_buftypes = { "terminal", "nofile" },
-	mappings = {
-		annotate = "m/",
-	},
-})
--- .config/nvim/lua/plugins/marks.lua
+++ .config/nvim/lua/plugins/marks.lua
@@ -1,5 +1,12 @@
 return {
 	"chentoast/marks.nvim",
 	event = "VeryLazy",
-	opts = {},
+	opts = {
+		-- builtin_marks = { "<", ">" },
+		builtin_marks = {},
+		excluded_buftypes = { "terminal", "nofile" },
+		mappings = {
+			annotate = "m/",
+		},
+	},
 }
